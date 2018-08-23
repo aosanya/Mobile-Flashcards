3c02d78642b0c5de5c1bf5511e223eddf424e039
aosanya <amosanya@gmail.com>
2018-08-23 20:21:59 +0200

    Data and Decks

---
+++ src/reducers/index.js
@@ -0,0 +1,21 @@
+import { combineReducers } from 'redux'
+import questions from '../services/poll/questions/reducer'
+import users from '../services/users/reducer'
+import authedUser from '../services/session/reducer'
+import {fetchingData, fetchedData} from '../services/status/reducer'
+import leaderBoard from '../services/poll/leaderBoard/reducer'
+
+const appReducer = combineReducers({
+  // authedUser,
+  // leaderBoard,
+  // questions,
+  // users,
+  // fetchingData,
+  // fetchedData,
+})
+
+const rootReducer = ( state, action ) => {
+  return appReducer(state, action)
+}
+
+export default rootReducer
